da256f183c32c5efe72b9ce057ed5b3130f88e81
tianyongwei <869358189@qq.com>
2018-03-16 17:09:48 +0800

    完成基本功能

--- background.js
+++ background.js
@@ -7,21 +7,38 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   
   var codeStr = '';
-  codeStr += 'console.log("hello");';
-  // var btnNodes = document.getElementsByClassName("Button--primary");
-  // for(var i=0;i<btnNodes.length;i++){
-  //   console.log(btnNodes[i].className);
-  //   console.log(btnNodes[i].className.replace("Button--primary",""));
-  //   btnNodes[i].className=btnNodes[i].className.replace("Button--primary","");
-  // };
-  codeStr = 'var btnNodes = document.getElementsByClassName("Button--primary");';
-  codeStr += 'for(var i=0;i<btnNodes.length;i++){console.log(btnNodes[i].className);console.log(btnNodes[i].className.replace("Button--primary",""));btnNodes[i].className=btnNodes[i].className.replace("Button--primary","")};';
+  codeStr += 'console.log("开始...");';
+  /**
+   * “低调化”处理
+   * 1、按钮去色
+   * 2、问题标题去掉加粗
+   * 3、点赞按钮去色
+   * 4、按钮的蓝色边框
+   */
+  codeStr += 'var classNames = new Array();';
+  codeStr += 'classNames.push("Button--primary");';//高亮按钮
+  codeStr += 'classNames.push("Button--blue");';//按钮-蓝色边框
+  codeStr += 'classNames.push("Button");';//按钮
+  codeStr += 'classNames.push("is-active");';//已赞按钮
+  codeStr += 'classNames.push("QuestionHeader-title");';//问题标题
+  codeStr += 'classNames.forEach(element => {console.log(element)});';
 
-  // codeStr = 'var titleNodes = document.getElementsByClassName("QuestionHeader-title");';
-  // codeStr += 'for(var i=0;i<titleNodes.length;i++){console.log(i);titleNodes[i].className=""};';
+  codeStr += 'classNames.forEach(element => {';
+  codeStr += '  console.log(document.getElementsByClassName(element).length);';
+  codeStr += '  var btnPrimaryLength = document.getElementsByClassName(element).length;';
+  codeStr += '  try{for(var i = 0;i<btnPrimaryLength;i++){document.getElementsByClassName(element)[0].className=document.getElementsByClassName(element)[0].className.replace(element,"")}}catch(err){console.log("隐藏Botton-primary出错！" + err)};';  
+  codeStr += '});';
 
-  // codeStr = 'var titleNodes2 = document.getElementsByClassName("QuestionHeader-title");';
-  // codeStr += 'for(var i=0;i<titleNodes2.length;i++){console.log(i);titleNodes2[i].className=""};';
+  /**
+   * 隐藏处理
+   * 1、隐藏知乎logo
+   * 2、隐藏相关推荐和相关问题
+   */
+  codeStr += 'document.getElementsByClassName("Icon--logo")[0].setAttribute("viewBox","0 0 0 0");';
+  codeStr += 'var cards = document.getElementsByClassName("Card");';
+  codeStr += 'for(var i=0;i<cards.length;i++){if("相关推荐" == cards[i].getAttribute("data-za-detail-view-path-module_name")) {cards[i].style.display="none"}if("RelatedQuestions" == cards[i].getAttribute("data-za-detail-view-path-module")) {cards[i].style.display="none"}};';
+
+  codeStr += 'console.log("结束...");';
   
   chrome.tabs.executeScript({
     code: codeStr
