2826b35a52deeb755dc957a69e4ae1700fef098c
tianyongwei <869358189@qq.com>
2018-08-03 10:18:28 +0800

    fix manifest

--- background.js
+++ background.js
@@ -4,7 +4,12 @@ chrome.runtime.onInstalled.addListener(function() {
       conditions: [new chrome.declarativeContent.PageStateMatcher({
         pageUrl: {hostContains: 'zhihu.com'},
       })],
-      actions: [new chrome.declarativeContent.RequestContentScript({js:["jquery.js","mute.js"]})]
+      actions: [
+        //不是知乎网站，图标变灰
+        new chrome.declarativeContent.ShowPageAction(),
+        //加载外部js，先加载jq，再mute.js，就可以在mute.js中使用jq
+        new chrome.declarativeContent.RequestContentScript({js:["jquery.js","mute.js"]})
+      ]
     }]);
   });
 });
